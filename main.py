98c712582b810672470e6f688e462b2784edffb6
Gunnar Babicz <gunnarbabicz@yahoo.com>
2026-07-26 21:50:44 -0600

    removed testing components and refactored main.py

--- main.py
+++ main.py
@@ -10,31 +10,29 @@ with open("config.json") as f:
 model = YOLO('yolov8n.pt')
 notifier = MeshtasticNotifier()
 
-# Open the test video
-print("this is before")
+# Open the video file or camera
 cap = cv2.VideoCapture(config["videoPath"])
-print("test")
 if not cap.isOpened():
     print("Error: Could not open camera. Please check port settings and try again")
     exit()
 
-while True:
+# While there are still frames to render, run this feed
+while True: 
     ret, frame = cap.read()
-    if not ret:
-        print("End of video reached.")
+
+    # If there is no longer video feed or the video ends, break
+    if not ret: 
         break
 
     # Run detection on this frame. Only detect people.
     results = model(frame, classes = [0], verbose=False)
 
     # If a person was detected, send a notification
-    
     if len(results[0].boxes) > 0 and notifier.can_send():
         sent = notifier.notify_person_detected()
         if sent:
             print("Alert sent.")
     
-
     # Draw bounding boxes, labels, and confidence scores onto the frame
     annotated_frame = results[0].plot()
 
